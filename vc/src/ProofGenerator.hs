004ecae34cdfaa940e6f38ed98d0014e76cb8fa9
Daniel Britten <daniel.britten@nethermind.io>
2024-11-18 04:06:57 +0000

    Improvements to generator: switch cases. By František
    Co-authored-by: Frantisek Silvasi <silvasi.frantisek@gmail.com>

--- vc/src/ProofGenerator.hs
+++ vc/src/ProofGenerator.hs
@@ -17,12 +17,12 @@ tacticsOfExpr (Var {}) = "-- simp [Var']"
 tacticsOfExpr (Lit {}) = "-- simp [Lit']"
 
 tacticsOfCond :: String
-tacticsOfCond = "simp [evalArgs, head', reverse', multifill', PrimCall', Lit', Var', execPrimCall, evalPrimCall]"
+tacticsOfCond = "try simp only [Fin.isValue]; try rw [List.foldr_cons]; simp [evalArgs, head', reverse', multifill', PrimCall', Lit', Var', execPrimCall, evalPrimCall]; try rewrite [List.foldr_nil]"
 
 assignCall :: String -> String
 assignCall name = unlines [
     "rw [cons]; simp only [LetCall', AssignCall']",
-    "simp [evalArgs, head', reverse', multifill', PrimCall', Lit', Var', execPrimCall, evalPrimCall]",
+    "try simp only [Fin.isValue]; try rw [List.foldr_cons]; simp [evalArgs, head', reverse', multifill', PrimCall', Lit', Var', execPrimCall, evalPrimCall]; try rewrite [List.foldr_nil]",
     "-- EXPR \6",
     "try simp",
     "generalize hs : execCall _ _ _ _ = s; try rw [← hs₁, hok] at hs",
@@ -84,7 +84,7 @@ tacticsOfStmt' _ (LetInit _ (Call f _)) =
     if f `elem` yulPrimOps
     then unlines [
           "rw [cons]; simp only [LetPrimCall', AssignPrimCall']",
-          "simp [evalArgs, head', reverse', multifill', PrimCall', Lit', Var', execPrimCall, evalPrimCall]",
+          "try simp only [Fin.isValue]; try rw [List.foldr_cons]; simp [evalArgs, head', reverse', multifill', PrimCall', Lit', Var', execPrimCall, evalPrimCall]; try rewrite [List.foldr_nil]",
           rwPrimop f,
           "try simp"]
     else assignCall f
@@ -93,7 +93,7 @@ tacticsOfStmt' _ (Assignment _ (Call f _)) =
   if f `elem` yulPrimOps
   then unlines [
          "rw [cons]; simp only [LetPrimCall', AssignPrimCall']",
-         "simp [evalArgs, head', reverse', multifill', PrimCall', Lit', Var', execPrimCall, evalPrimCall]",
+         "try simp only [Fin.isValue]; try rw [List.foldr_cons]; simp [evalArgs, head', reverse', multifill', PrimCall', Lit', Var', execPrimCall, evalPrimCall]; try rewrite [List.foldr_nil]",
          rwPrimop f,
          "try simp"]
   else assignCall f
@@ -103,13 +103,13 @@ tacticsOfStmt' _ (ExpressionStmt (Call f args)) =
   if f `elem` yulPrimOps
   then unlines [
     "rw [cons, ExprStmtPrimCall']; try simp only",
-    "simp [evalArgs, head', reverse', multifill', PrimCall', Lit', Var', execPrimCall, evalPrimCall]",
+    "try simp only [Fin.isValue]; try rw [List.foldr_cons]; simp [evalArgs, head', reverse', multifill', PrimCall', Lit', Var', execPrimCall, evalPrimCall]; try rewrite [List.foldr_nil]",
     "-- EXPR \4",
     rwPrimop f,
     "try simp"]
   else let preamble = unlines [
              "rw [cons, ExprStmtCall']; try simp only",
-             "simp [evalArgs, head', reverse', multifill', PrimCall', Lit', Var', execPrimCall, evalPrimCall]"]
+             "try simp only [Fin.isValue]; try rw [List.foldr_cons]; simp [evalArgs, head', reverse', multifill', PrimCall', Lit', Var', execPrimCall, evalPrimCall]; try rewrite [List.foldr_nil]"]
            postamble = unlines [
              "try simp",
              "",
