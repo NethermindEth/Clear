575279f3ada69edbd4df607ea524a06b47e160f3
Daniel Britten <Coda-Coda@users.noreply.github.com>
2024-11-29 07:52:49 +0000

    Add spec and proof for `msgSender`

--- Generated/erc20shim/ERC20Shim/fun_msgSender_user.lean
+++ Generated/erc20shim/ERC20Shim/fun_msgSender_user.lean
@@ -10,13 +10,43 @@ section
 
 open Clear EVMState Ast Expr Stmt FunctionDefinition State Interpreter ExecLemmas OutOfFuelLemmas Abstraction YulNotation PrimOps ReasoningPrinciple Utilities 
 
-def A_fun_msgSender (var : Identifier)  (s₀ s₉ : State) : Prop := sorry
+def A_fun_msgSender (var : Identifier)  (s₀ s₉ : State) : Prop := 
+  preservesEvm s₀ s₉ ∧
+  s₉.isOk ∧
+  (s₀.evm.isEVMState → s₉.evm.isEVMState) ∧
+  (s₀.evm.hash_collision = true → s₉.evm.hash_collision) ∧ 
+  (
+    (
+      match s₀ with
+      | Ok evm _ => s₉[var]!! = evm.execution_env.source ∧
+                    s₉.evm.hash_collision = false
+      | _ => s₉.evm.hash_collision = false -- OutOfFuel or Checkpoint
+    )
+    -- Case: Hash collision
+    ∨ s₉.evm.hash_collision = true 
+  )
 
 lemma fun_msgSender_abs_of_concrete {s₀ s₉ : State} {var } :
   Spec (fun_msgSender_concrete_of_code.1 var ) s₀ s₉ →
   Spec (A_fun_msgSender var ) s₀ s₉ := by
   unfold fun_msgSender_concrete_of_code A_fun_msgSender
-  sorry
+  rcases s₀ with ⟨evm₀, varstore₀⟩ | _ | _ <;> [simp only; aesop_spec; aesop_spec]
+  apply spec_eq
+  clr_funargs
+  rintro hasFuel code
+  simp at code
+  rw [←code]
+  simp
+  
+  split_ands
+  · unfold preservesEvm
+    rw [← State.insert_of_ok, lookup_insert, State.insert_of_ok]
+    simp
+  · by_cases h : evm₀.hash_collision <;> [(right;assumption);skip]
+    left
+    split_ands <;> [skip;aesop]
+    unfold lookup!
+    simp
 
 end
 
