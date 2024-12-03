9b20b3f5c5bfd842ee07d20a40d5212435f492aa
Daniel Britten <Coda-Coda@users.noreply.github.com>
2024-12-03 03:00:02 +0000

    Improve `msgSender` spec and proof

--- Generated/erc20shim/ERC20Shim/fun_msgSender_user.lean
+++ Generated/erc20shim/ERC20Shim/fun_msgSender_user.lean
@@ -11,20 +11,13 @@ section
 open Clear EVMState Ast Expr Stmt FunctionDefinition State Interpreter ExecLemmas OutOfFuelLemmas Abstraction YulNotation PrimOps ReasoningPrinciple Utilities 
 
 def A_fun_msgSender (var : Identifier)  (s₀ s₉ : State) : Prop := 
-  preservesEvm s₀ s₉ ∧
+  ((preservesEvm s₀ s₉ ∧
   s₉.isOk ∧
   (s₀.evm.isEVMState → s₉.evm.isEVMState) ∧
-  (s₀.evm.hash_collision = true → s₉.evm.hash_collision) ∧ 
-  (
-    (
-      match s₀ with
-      | Ok evm _ => s₉[var]!! = evm.execution_env.source ∧
-                    s₉.evm.hash_collision = false
-      | _ => s₉.evm.hash_collision = false -- OutOfFuel or Checkpoint
-    )
-    -- Case: Hash collision
-    ∨ s₉.evm.hash_collision = true 
-  )
+  s₉[var]!! = s₀.evm.execution_env.source ∧
+  s₉.evm.hash_collision = false)
+  ∨ s₉.evm.hash_collision = true)
+  ∧ (s₀.evm.hash_collision = true → s₉.evm.hash_collision) 
 
 lemma fun_msgSender_abs_of_concrete {s₀ s₉ : State} {var } :
   Spec (fun_msgSender_concrete_of_code.1 var ) s₀ s₉ →
@@ -36,17 +29,10 @@ lemma fun_msgSender_abs_of_concrete {s₀ s₉ : State} {var } :
   rintro hasFuel code
   simp at code
   rw [←code]
+  unfold preservesEvm
+  rw [← State.insert_of_ok, lookup_insert, State.insert_of_ok]
+  unfold lookup!
   simp
-  
-  split_ands
-  · unfold preservesEvm
-    rw [← State.insert_of_ok, lookup_insert, State.insert_of_ok]
-    simp
-  · by_cases h : evm₀.hash_collision <;> [(right;assumption);skip]
-    left
-    split_ands <;> [skip;aesop]
-    unfold lookup!
-    simp
 
 end
 
