55483c1eb556045b88760a214c78c6fa1fd052ac
Daniel Britten <Coda-Coda@users.noreply.github.com>
2025-05-12 02:41:29 +0000

    Tweak specification and prove `mapping_index_access_mapping_address_uint256_of_address`
    Bringing in changes from https://github.com/EllbellCode/Clear_fork/tree/erc20_Elliot
    
    Co-authored-by: EllbellCode <ellbellcode@gmail.com>

--- Generated/erc20shim/ERC20Shim/mapping_index_access_mapping_address_uint256_of_address_user.lean
+++ Generated/erc20shim/ERC20Shim/mapping_index_access_mapping_address_uint256_of_address_user.lean
@@ -10,7 +10,7 @@ namespace Generated.erc20shim.ERC20Shim
 
 section
 
-open Clear EVMState Ast Expr Stmt FunctionDefinition State Interpreter ExecLemmas OutOfFuelLemmas Abstraction YulNotation PrimOps ReasoningPrinciple Utilities 
+open Clear EVMState Ast Expr Stmt FunctionDefinition State Interpreter ExecLemmas OutOfFuelLemmas Abstraction YulNotation PrimOps ReasoningPrinciple Utilities
 
 abbrev AddressMap := Finmap (λ _ : Address ↦ UInt256)
 
@@ -18,12 +18,22 @@ set_option linter.setOption false
 set_option pp.coercions false
 
 def A_mapping_index_access_mapping_address_uint256_of_address (dataSlot : Identifier) (slot key : Literal) (s₀ s₉ : State) : Prop :=
-  ((preservesEvm s₀ s₉ ∧ s₉.isOk ∧ (s₀.evm.isEVMState → s₉.evm.isEVMState) ∧ (∃ keccak,
-  s₉.evm.keccak_map.lookup [ ↑(Address.ofUInt256 key), slot ] = some keccak ∧
-  s₉.store = s₀⟦dataSlot ↦ keccak⟧.store) ∧
-  s₉.evm.hash_collision = false)
-  ∨ s₉.evm.hash_collision = true)
-  ∧ (s₀.evm.hash_collision = true → s₉.evm.hash_collision = true)
+  s₉.isOk
+  ∧
+  (
+    (
+      preservesEvm s₀ s₉ ∧
+      (s₀.evm.isEVMState → s₉.evm.isEVMState) ∧
+      (∃ keccak,
+        s₉.evm.keccak_map.lookup [ ↑(Address.ofUInt256 key), slot ] = some keccak ∧
+        s₉.store = s₀⟦dataSlot ↦ keccak⟧.store
+      ) ∧
+      s₉.evm.hash_collision = false
+    )
+    ∨ s₉.evm.hash_collision = true
+  )
+  ∧
+  (s₀.evm.hash_collision = true → s₉.evm.hash_collision = true)
 
 -- Helper reifications
 lemma shift_eq_size : Fin.shiftLeft (n := UInt256.size) 1 160 = Address.size := by
@@ -41,7 +51,7 @@ lemma mapping_index_access_mapping_address_uint256_of_address_abs_of_concrete {s
   apply spec_eq
   intro hasFuel
   clr_funargs
-  
+
   rw [ EVMSub', EVMShl', EVMAddrSize' ]; simp
   rw [ Address.and_size_eq_ofUInt256 ]
   rw [ multifill_cons, multifill_nil ]
@@ -67,93 +77,89 @@ lemma mapping_index_access_mapping_address_uint256_of_address_abs_of_concrete {s
     have prep_collision : state_prep.hash_collision = evm.hash_collision := by
       rw [← prep_def]
       exact Eq.trans hash_collision_of_mstore hash_collision_of_mstore
-      
+
     have preserves_collision :
-      evm.hash_collision = true → Ok res.2 varstore⟦dataSlot↦res.1⟧.evm.hash_collision = true := by      
+      evm.hash_collision = true → Ok res.2 varstore⟦dataSlot↦res.1⟧.evm.hash_collision = true := by
       rw [State.insert_of_ok, get_evm_of_ok, res_collision, prep_collision]
       intro h; exact h
-    
+
     apply And.intro
-    swap; exact preserves_collision
-    
-    by_cases h : evm.hash_collision
-    right
-    -- hash_collision from the previous state
-    exact preserves_collision h
-    
-    -- no hash collision from the previous state
-    left; split_ands
-    -- preservesEvm
-    rw [preservesEvm_of_insert']
-    apply preservesEvm_of_preserved
-    rw [get_evm_of_ok, get_evm_of_ok]
-    exact Preserved.trans Preserved_prep (Preserved_of_keccek256 keccak_eq_some_res)
 
-    -- state is ok
-    exact State.isOk_Ok
-    -- s₉.evm.isEVMState
-    intro hIsEVMState
-    obtain ⟨res₁,res₂⟩ := res
-    simp
-    
-    have state_prep_isEVMState : isEVMState state_prep := by
-      rw [←prep_def]
-      
-      unfold isEVMState
-      split_ands
-      unfold isKeccakInjective
-      simp
-      intros a b h₁ h₂
-      rw [mstore_preserves_keccak_map, mstore_preserves_keccak_map] at h₂
-      unfold isEVMState at hIsEVMState
-      aesop
-      
-      unfold isKeccakUsedRange
-      simp  
-      rw [mstore_preserves_keccak_map, mstore_preserves_keccak_map,
-          mstore_preserves_used_range, mstore_preserves_used_range]
-      unfold isEVMState at hIsEVMState
-      aesop
-    
-    apply @isEVMState_of_keccak256 ⟨state_prep, state_prep_isEVMState⟩ _ _ _ _ keccak_eq_some_res
-    
-    -- keccak
-    use res.1
-    split_ands
-    -- keccak lookup for last
-    rotate_left
-    -- varstore preservation
-    rw [State.insert_of_ok]
-    simp only [State.store]
-    
-    -- no hash collision
-    rw [State.insert_of_ok, get_evm_of_ok, res_collision, prep_collision]
-    rw [Bool.eq_false_eq_not_eq_true] at h; exact h
-    
-    -- keccak lookup
-    rw [State.insert_of_ok, get_evm_of_ok]
-    unfold keccak256 at keccak_eq_some_res
-    rw [ interval'_eq_interval 2 two_ne_zero (by norm_cast)
-       , ← prep_def
-       , mstore_mstore_of_ne (by sorry), interval_of_mstore_eq_val_cons
-       , mstore_mstore_of_ne (by sorry), zero_add, interval_of_mstore_eq_val_cons
-       , interval_of_0_eq_nil
-       ] at keccak_eq_some_res
-    unfold_let at keccak_eq_some_res
+    · aesop
+    · split_ands
+      · by_cases h : evm.hash_collision
+        · right
+          exact preserves_collision h
+        ·
+          left; split_ands
+          ·  -- preservesEvm
+            rw [preservesEvm_of_insert']
+            apply preservesEvm_of_preserved
+            rw [get_evm_of_ok, get_evm_of_ok]
+            exact Preserved.trans Preserved_prep (Preserved_of_keccek256 keccak_eq_some_res)
+          ·  -- s₉.evm.isEVMState
+            intro hIsEVMState
+            obtain ⟨res₁,res₂⟩ := res
+            simp
 
-    split at keccak_eq_some_res
-    case h_1 _ v h_lookup =>
-      rw [Option.some_inj] at keccak_eq_some_res
-      rw [← keccak_eq_some_res]
-      exact h_lookup
-    case h_2 _ h_lookup =>
-      split at keccak_eq_some_res
-      swap; contradiction
-      rw [Option.some_inj] at keccak_eq_some_res
-      rw [← keccak_eq_some_res]
-      simp only [];
-      rw [Finmap.lookup_insert]
+            have state_prep_isEVMState : isEVMState state_prep := by
+              rw [←prep_def]
 
+              unfold isEVMState
+              split_ands
+              unfold isKeccakInjective
+              simp
+              intros a b h₁ h₂
+              rw [mstore_preserves_keccak_map, mstore_preserves_keccak_map] at h₂
+              unfold isEVMState at hIsEVMState
+              aesop
+
+              unfold isKeccakUsedRange
+              simp
+              rw [mstore_preserves_keccak_map, mstore_preserves_keccak_map,
+                  mstore_preserves_used_range, mstore_preserves_used_range]
+              unfold isEVMState at hIsEVMState
+              aesop
+
+            apply @isEVMState_of_keccak256 ⟨state_prep, state_prep_isEVMState⟩ _ _ _ _ keccak_eq_some_res
+
+          -- keccak
+          · use res.1
+            split_ands
+            -- keccak lookup for last
+            rotate_left
+            -- varstore preservation
+            rw [State.insert_of_ok]
+            simp only [State.store]
+            rw [State.insert_of_ok, get_evm_of_ok]
+            unfold keccak256 at keccak_eq_some_res
+            rw [ interval'_eq_interval 2 two_ne_zero (by norm_cast)
+              , ← prep_def
+              , mstore_mstore_of_ne (by sorry)
+              , interval_of_mstore_eq_val_cons
+              , mstore_mstore_of_ne (by sorry)
+              , zero_add, interval_of_mstore_eq_val_cons
+              , interval_of_0_eq_nil
+              ] at keccak_eq_some_res
+            unfold_let at keccak_eq_some_res
+
+            split at keccak_eq_some_res
+            case h_1 _ v h_lookup =>
+              rw [Option.some_inj] at keccak_eq_some_res
+              rw [← keccak_eq_some_res]
+              exact h_lookup
+            case h_2 _ h_lookup =>
+              split at keccak_eq_some_res
+              swap; contradiction
+              rw [Option.some_inj] at keccak_eq_some_res
+              rw [← keccak_eq_some_res]
+              simp only [];
+              rw [Finmap.lookup_insert]
+
+          ·-- no hash collision
+            rw [State.insert_of_ok, get_evm_of_ok, res_collision, prep_collision]
+            rw [Bool.eq_false_eq_not_eq_true] at h; exact h
+      · aesop
   case h_2 res keccak_eq_none =>
     clr_match at code
 
@@ -162,8 +168,8 @@ lemma mapping_index_access_mapping_address_uint256_of_address_abs_of_concrete {s
       exact hash_collision_of_addHashCollision state_prep
 
     apply And.intro
-    right; exact final_destination
-    intro _; exact final_destination
+    · aesop
+    · aesop
 
   end
 
